c618c425b3946856468e76e970a527384d1248dc
Killian Clette <killian.clette@gmail.com>
2020-05-09 15:56:32 +0200

    rand: adding debug logs

--- calibration-app/app/src/main/java/org/dpppt/android/calibration/MainApplication.java
+++ calibration-app/app/src/main/java/org/dpppt/android/calibration/MainApplication.java
@@ -47,7 +47,7 @@ public class MainApplication extends Application {
 				"LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0R" +
 						"RZ0FFWTc3MFZEWjJlZjZCYjh0UXZYWVJpcUFaemtHLwpwNWs0U3pTV3FRY00zNzlqTVN6c3JOaU5nc0" +
 						"hWZlRPeGFqMUFzQ3RrNmJVUDV1cDc3RU5nckVzVkh3PT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0t");
-		DP3T.init(context, "org.dpppt.demo", false, publicKey);
+		DP3T.init(context, "org.dpppt.demo", true, publicKey);
 
 		CertificatePinner certificatePinner = new CertificatePinner.Builder()
 				.add("demo.dpppt.org", "sha256/YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg=")
