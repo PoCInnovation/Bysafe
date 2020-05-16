7a67ab762174e83e396bbf93b53b0d8f05c08c41
ltsrc <robin.christol@epitech.eu>
2020-05-16 18:59:25 +0200

    added app bar and icons

--- calibration-app/app/src/main/java/org/dpppt/android/calibration/MainActivity.java
+++ calibration-app/app/src/main/java/org/dpppt/android/calibration/MainActivity.java
@@ -11,60 +11,54 @@
 package org.dpppt.android.calibration;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.dpppt.android.calibration.controls.ControlsFragment;
-import org.dpppt.android.calibration.handshakes.HandshakesFragment;
-import org.dpppt.android.calibration.logs.LogsFragment;
 import org.dpppt.android.calibration.parameters.ParametersFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		setupNavigationView();
+        setupNavigationView();
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.main_fragment_container, ParametersFragment.newInstance())//ControlsFragment.newInstance())
-					.commit();
-		}
-	}
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_fragment_container, ParametersFragment.newInstance())
+                    .commit();
+        }
+    }
 
-	private void setupNavigationView() {
-		BottomNavigationView navigationView = findViewById(R.id.main_navigation_view);
-		navigationView.inflateMenu(R.menu.menu_navigation_main);
+    private void setupNavigationView() {
+        BottomNavigationView navigationView = findViewById(R.id.main_navigation_view);
+        navigationView.inflateMenu(R.menu.menu_navigation_main);
 
-		navigationView.setOnNavigationItemSelectedListener(item -> {
-			switch (item.getItemId()) {
-//				case R.id.action_controls:
-//					getSupportFragmentManager().beginTransaction()
-//							.replace(R.id.main_fragment_container, ControlsFragment.newInstance())
-//							.commit();
-//					break;
-				case R.id.action_parameters:
-					getSupportFragmentManager().beginTransaction()
-							.replace(R.id.main_fragment_container, ParametersFragment.newInstance())
-							.commit();
-					break;
-				case R.id.action_handshakes:
-					getSupportFragmentManager().beginTransaction()
-							.replace(R.id.main_fragment_container, HandshakesFragment.newInstance())
-							.commit();
-					break;
-//				case R.id.action_logs:
-//					getSupportFragmentManager().beginTransaction()
-//							.replace(R.id.main_fragment_container, LogsFragment.newInstance())
-//							.commit();
-//					break;
-			}
-			return true;
-		});
-	}
+        navigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_parameters:
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_fragment_container, ParametersFragment.newInstance())
+                            .commit();
+                    break;
+                case R.id.action_handshakes:
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_fragment_container, ParametersFragment.newInstance())
+                            .commit();
+                    break;
+                case R.id.action_activities:
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_fragment_container, ParametersFragment.newInstance())
+                            .commit();
+                    break;
+            }
+            return true;
+        });
+    }
 
 }
