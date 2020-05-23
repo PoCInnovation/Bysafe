9f3a91046d07bdc688a0eceefacbeac69215761e
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2020-05-23 15:27:49 +0200

    Added notification on wash countdown

--- calibration-app/app/src/main/java/org/dpppt/android/calibration/handwash/HandwashFragment.java
+++ calibration-app/app/src/main/java/org/dpppt/android/calibration/handwash/HandwashFragment.java
@@ -1,5 +1,12 @@
 package org.dpppt.android.calibration.handwash;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -7,12 +14,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
 import org.dpppt.android.calibration.R;
-import org.dpppt.android.sdk.internal.logger.Logger;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,6 +33,13 @@ public class HandwashFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        Context context = getContext();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            String channelName = context.getString(R.string.app_name);
+            NotificationChannel channel = new NotificationChannel(channelName, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
         return inflater.inflate(R.layout.fragment_handwash, container, false);
     }
 
@@ -42,6 +58,26 @@ public class HandwashFragment extends Fragment {
         return new HandwashFragment();
     }
 
+    public static void showNotification(Context context, @StringRes int title, @StringRes int message, @DrawableRes int icon) {
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
+        PendingIntent contentIntent = null;
+        if (launchIntent != null) {
+            contentIntent = PendingIntent.getActivity(context, 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+        Notification notification =
+                new NotificationCompat.Builder(context, context.getString(R.string.dp3t_sdk_service_notification_channel))
+                        .setContentTitle(context.getString(title))
+                        .setContentText(context.getString(message))
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
+                        .setSmallIcon(icon)
+                        .setContentIntent(contentIntent)
+                        .setAutoCancel(true)
+                        .build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(2, notification);
+    }
+
     private Runnable timerTask = new Runnable() {
         public void run() {
             long millis = TimeUnit.SECONDS.toMillis(5) - (System.currentTimeMillis() - lastWashedTime);
@@ -53,11 +89,14 @@ public class HandwashFragment extends Fragment {
             millis -= TimeUnit.MINUTES.toMillis(minutes);
             long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
 
-            Logger.d("D/WASH", "" + hours + ":" + minutes + ":" + seconds);
+            final Button button = getView().findViewById(R.id.washed_hands);
+            button.setText("" + hours + ":" + minutes + ":" + seconds);
 
             if (seconds > 0){
                 handler.postDelayed(this, 1000);
             } else {
+                button.setText(getContext().getString(R.string.button_wash));
+                showNotification(getContext(), R.string.app_name,  R.string.wash_alert, R.drawable.ic_bysafe);
             }
         }
     };
