cbf66cc3dcc2cdcac8dfec4dde2e2adfe8e4e7aa
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2020-06-06 14:56:29 +0200

    Added firebase-config

--- front/admin/firebase-config-example.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: '<API_KEY>',
-    authDomain: '<PROJECT_ID>.firebaseapp.com',
-    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
-    projectId: '<PROJECT_ID>',
-    storageBucket: '<BUCKET>.appspot.com',
-    messagingSenderId: '<SENDER_ID>',
-};
-firebase.initializeApp(config);
---
+++ front/admin/firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBVB7aWXmwGpAWxJ0LBA_T2nh6BiSYePyM",
+    authDomain: "bysafe-4ee9a.firebaseapp.com",
+    databaseURL: "https://bysafe-4ee9a.firebaseio.com",
+    projectId: "bysafe-4ee9a",
+    storageBucket: "bysafe-4ee9a.appspot.com",
+    messagingSenderId: "980222166759",
+};
+firebase.initializeApp(config);
