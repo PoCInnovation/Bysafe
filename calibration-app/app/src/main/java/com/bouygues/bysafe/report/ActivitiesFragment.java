7159a078b48c8354691b284e43fa6c7aad0bc978
ltsrc <robin.christol@epitech.eu>
2020-06-22 23:01:17 +0200

    added: a new screen to display the list in activities tab

--- calibration-app/app/src/main/java/com/bouygues/bysafe/report/ActivitiesFragment.java
+++ calibration-app/app/src/main/java/com/bouygues/bysafe/report/ActivitiesFragment.java
@@ -15,9 +15,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import org.dpppt.android.sdk.internal.util.Pair;
 
 import androidx.annotation.NonNull;
@@ -27,6 +29,7 @@ import androidx.fragment.app.Fragment;
 import com.bouygues.bysafe.MainActivity;
 import com.bouygues.bysafe.MainApplication;
 import com.bouygues.bysafe.R;
+import com.bouygues.bysafe.handwash.BarrierGestureInfoFragment;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 import org.dpppt.android.sdk.internal.AppConfigManager;
@@ -53,9 +56,11 @@ public class ActivitiesFragment extends Fragment {
     private TextView percentage_header;
     private ImageView unhappyMasked;
     private ImageView happyMasked;
+    private Button reportButton;
     private long interval = 60000;
-    private ArrayList<String> list = new ArrayList<>();
-    private ArrayList<android.widget.TextView> listId = new ArrayList<>();
+
+    //    private ArrayList<String> list = new ArrayList<>();
+//    private ArrayList<android.widget.TextView> listId = new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -71,79 +76,38 @@ public class ActivitiesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        listId.add(view.findViewById(R.id.Hour8));
-        listId.add(view.findViewById(R.id.Hour9));
-        listId.add(view.findViewById(R.id.Hour10));
-        listId.add(view.findViewById(R.id.Hour11));
-        listId.add(view.findViewById(R.id.Hour12));
-        listId.add(view.findViewById(R.id.Hour13));
-        listId.add(view.findViewById(R.id.Hour14));
-        listId.add(view.findViewById(R.id.Hour15));
-        listId.add(view.findViewById(R.id.Hour16));
-        listId.add(view.findViewById(R.id.Hour17));
-        listId.add(view.findViewById(R.id.Hour18));
-
         pb = view.findViewById(R.id.day_report_circular_progress_bar);
         percentage_header = view.findViewById(R.id.text_percentage_header);
         happyMasked = view.findViewById(R.id.activities_happy_masked);
         unhappyMasked = view.findViewById(R.id.activities_unhappy_masked);
+        reportButton = view.findViewById(R.id.go_to_report_list);
+
+        reportButton.setOnClickListener(v -> {
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_fragment_container, ActivitiesReportFragment.newInstance())
+                    .commit();
+        });
+
         getJourneyPercentage();
-
-
-        //ListView ll = view.findViewById(R.id.log_list);
-        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(view.getContext(), R.layout.row, R.id.row_text, list);
-        //ll.setAdapter(arrayAdapter);
     }
 
     private void getJourneyPercentage() {
         try {
             ArrayList<Triplet<Long, Integer, String>> journeyContact = AppConfigManager.getInstance(MainApplication.getContext()).getJourneyContact();
-            SimpleDateFormat formater = new SimpleDateFormat("HH:mm");
             long total = journeyContact.size();
-            float totalTime = (float)total / (float)12;
+            float totalTime = (float) total / (float) 12;
             if (total == 0)
                 total = 1;
             long contacts = 0;
 
-            SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
-            for (long i = atStartOfDay(new java.util.Date()); i <= atEndOfDay(new java.util.Date()); i += 3600000) {
-                stringBuilder.append(formater.format(i)).append("   ");
-                for (long j = i; j < i + 3600000; j += 300000) {
-                    boolean set = false;
-                    for (Triplet<Long, Integer, String> interval: journeyContact) {
-                        if (interval.first >= j && interval.first < j + 300000 && interval.third.equals(AppConfigManager.getInstance(MainApplication.getContext()).getPrefBadgeNumber())) {
-                            if (interval.second > 0) {
-                                stringBuilder.append("V");
-                                stringBuilder.setSpan(new BackgroundColorSpan(Color.RED),stringBuilder.length() - 1, stringBuilder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                                stringBuilder.setSpan(new ForegroundColorSpan(Color.RED),stringBuilder.length() - 1, stringBuilder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                            } else {
-                                stringBuilder.append("V");
-                                stringBuilder.setSpan(new BackgroundColorSpan(Color.GREEN),stringBuilder.length() - 1, stringBuilder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                                stringBuilder.setSpan(new ForegroundColorSpan(Color.GREEN),stringBuilder.length() - 1, stringBuilder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                            }
-                            set = true;
-                        }
-                    }
-                    if (!set) {
-                        stringBuilder.append("V");
-                        stringBuilder.setSpan(new BackgroundColorSpan(Color.LTGRAY), stringBuilder.length() - 1, stringBuilder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        stringBuilder.setSpan(new ForegroundColorSpan(Color.LTGRAY),stringBuilder.length() - 1, stringBuilder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    }
-                }
-                long x = (i - atStartOfDay(new java.util.Date())) / 3600000;
-                listId.get((int)x).setText(stringBuilder);
-                list.add(stringBuilder.toString());
-                stringBuilder.clear();
-                stringBuilder.clearSpans();
-            }
-            for (Triplet<Long, Integer, String> interval: journeyContact) {
+            for (Triplet<Long, Integer, String> interval : journeyContact) {
                 if (interval.second > 0 && interval.third.equals(AppConfigManager.getInstance(MainApplication.getContext()).getPrefBadgeNumber())) {
                     contacts += 1;
                 }
             }
             String toDisplay = String.format("Pourcentage d'exposition de la journée (sur %.1f heures): \n", totalTime);
             percentage_header.setText(toDisplay);
-            float percent = ((((float)(contacts)) / ((float)total)) * 100);
+            float percent = ((((float) (contacts)) / ((float) total)) * 100);
             if (percent >= 10) {
                 pb.setProgressBarColor(Color.parseColor("#FA990C"));
                 happyMasked.setVisibility(View.GONE);
@@ -157,25 +121,5 @@ public class ActivitiesFragment extends Fragment {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public long atEndOfDay(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 18);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar.getTimeInMillis();
-    }
-
-    private long atStartOfDay(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 8);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar.getTimeInMillis();
     }
 }
