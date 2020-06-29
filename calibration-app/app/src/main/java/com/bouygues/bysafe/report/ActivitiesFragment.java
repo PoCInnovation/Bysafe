473925846431590d92be134ee487cb8abd74aeee
ltsrc <robin.christol@epitech.eu>
2020-06-29 10:20:01 +0200

    changed button ui

--- calibration-app/app/src/main/java/com/bouygues/bysafe/report/ActivitiesFragment.java
+++ calibration-app/app/src/main/java/com/bouygues/bysafe/report/ActivitiesFragment.java
@@ -1,58 +1,33 @@
 package com.bouygues.bysafe.report;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import org.dpppt.android.sdk.internal.util.Pair;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.bouygues.bysafe.MainActivity;
 import com.bouygues.bysafe.MainApplication;
 import com.bouygues.bysafe.R;
-import com.bouygues.bysafe.handwash.BarrierGestureInfoFragment;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 import org.dpppt.android.sdk.internal.AppConfigManager;
-import org.dpppt.android.sdk.internal.database.Database;
-import org.dpppt.android.sdk.internal.database.models.Handshake;
 import org.dpppt.android.sdk.internal.logger.Logger;
 import org.dpppt.android.sdk.internal.util.Triplet;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 public class ActivitiesFragment extends Fragment {
 
@@ -95,7 +70,7 @@ public class ActivitiesFragment extends Fragment {
         happyIc = view.findViewById(R.id.activities_status_ok_ic);
         unhappyIc = view.findViewById(R.id.activities_status_alert_ic);
 
-        Button reportButton = view.findViewById(R.id.go_to_report_list);
+        TextView reportTv = view.findViewById(R.id.go_to_report_list);
         ImageButton manager = view.findViewById(R.id.button_manager_view);
 
         if (AppConfigManager.getInstance(getContext()).getPrefManager()) {
@@ -104,7 +79,7 @@ public class ActivitiesFragment extends Fragment {
                     .replace(R.id.main_fragment_container, TeamActivitiesFragment.newInstance()).commit());
         }
 
-        reportButton.setOnClickListener(v -> getActivity().getSupportFragmentManager().beginTransaction()
+        reportTv.setOnClickListener(v -> getActivity().getSupportFragmentManager().beginTransaction()
                 .replace(R.id.main_fragment_container, ActivitiesReportFragment.newInstance())
                 .commit());
 
