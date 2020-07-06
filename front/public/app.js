4af8082e57942e5a2a249f7d9bebc098afe64b1e
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2020-07-06 21:50:40 +0200

    Added more CSS

--- front/public/app.js
+++ front/public/app.js
@@ -9,14 +9,14 @@ const getElem = (id) => document.getElementById(id);
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        getElem('login').hidden = true;
-        getElem('logged_in').hidden = false;
+        getElem('auth_section').hidden = true;
+        getElem('admin_section').hidden = false;
         user.getIdToken(true).then((idToken) => {
             global_idToken = idToken;
         });
     } else {
-        getElem('login').hidden = false;
-        getElem('logged_in').hidden = true;
+        getElem('auth_section').hidden = false;
+        getElem('admin_section').hidden = true;
         global_idToken = null;
     }
 });
@@ -32,7 +32,7 @@ const login = () => {
     signOut();
 
     const email = 'admin@bysafe.app';
-    const password = getElem('password').value;
+    const password = getElem('login_password').value;
 
     firebase.auth().signInWithEmailAndPassword(email, password).catch(console.error);
 };
@@ -40,43 +40,67 @@ const login = () => {
 const queryString = () => '?token=' + global_idToken;
 
 const validateForm = () => {
-    getElem('upload_form').action = addUsersEndpoint + queryString();
+    getElem('file_form').action = addUsersEndpoint + queryString();
 };
 
 const deleteUser = () => {
-    getElem('delete_button').disabled = true;
     const id = getElem('delete_id').value;
+    if (id === '') return;
+
+    getElem('delete_button').disabled = true;
     const url = deleteUserEndpoint + id + queryString();
 
     fetch(url, { method: 'POST', mode: 'no-cors' })
         .catch(console.error)
         .finally(() => {
+            getElem('delete_id').value = '';
             getElem('delete_button').disabled = false;
         });
 };
 
 const refreshUsers = () => {
-    getElem('refresh_db').textContent = 'Rafraîchissement en cours';
+    getElem('db_refresh').disabled = true;
+    getElem('db_refresh').textContent = 'Rafraîchissement en cours';
 
-    getElem('all_db').innerHTML = '';
+    getElem('db_content').innerHTML = '';
 
     fetch(getUsersEndpoint + queryString())
         .then((r) =>
             r.json().then((ret) => {
                 ret.sort((l, r) => l.id - r.id).forEach(({ id, firstname, lastname, manager }) => {
                     let value = `<div class="db_elem">👤 ${id} - ${firstname} ${lastname}<br>👥 ${manager}</div>`;
-                    getElem('all_db').innerHTML += value;
+                    getElem('db_content').innerHTML += value;
                 });
 
-                getElem('refresh_db').textContent = 'Rafraîchir';
-                getElem('info_db').textContent =
+                getElem('db_refresh').textContent = 'Rafraîchir';
+                getElem('db_refresh').disabled = false;
+                getElem('db_info').textContent =
                     'Dernier rafraîchissement le ' +
                     new Date().toISOString().substring(0, 19).replace('T', ' à ');
             })
         )
         .catch((e) => {
             console.error(e);
-            getElem('refresh_db').textContent = 'Rafraîchir';
-            getElem('info_db').textContent = 'Erreur de rafraîchissement';
+            getElem('db_refresh').textContent = 'Rafraîchir';
+            getElem('db_refresh').disabled = false;
+            getElem('db_info').textContent = 'Erreur de rafraîchissement';
         });
 };
+
+window.addEventListener('DOMContentLoaded', () => {
+    getElem('file_input').addEventListener('change', (e) => {
+        var inputText = '';
+        const filesNb = e.target.files.length;
+
+        if (filesNb == 0) {
+            inputText = `Aucun fichier selectionné`;
+            getElem('file_submit').disabled = true;
+        } else {
+            if (filesNb == 1) inputText = e.target.files[0].name;
+            else inputText = `${filesNb} fichiers selectionnés`;
+            getElem('file_submit').disabled = false;
+        }
+
+        getElem('file_select').innerHTML = inputText;
+    });
+});
